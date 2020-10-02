2369d06bfc5fd24aa8ccdf26c3e67b8f4287f1af
yagogomes792 <yago.792@hotmail.com>
2020-10-02 11:02:08 -0300

    alterado templates de login e register. Alterado Front

--- blog_app/views.py
+++ blog_app/views.py
@@ -94,4 +94,4 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-        return redirect(reverse('dashboard'))
+        return redirect(reverse('home'))
